a25ec86f16ea65a29f2a861cb80200489c88d20a
AmanAsharma070320 <asharma070320@gmail.com>
2024-06-01 12:28:42 +0530

    Weather App

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -84,7 +84,7 @@ export default function Home() {
     "repoData",
     async () => {
       const { data } = await axios.get(
-        `https://api.openweathermap.org/data/2.5/forecast?q=${place}&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}&cnt=56`
+        `https://api.openweathermap.org/data/2.5/forecast?q=${place}&appid=42187abaa73e8583284b42472d2542e7&cnt=56`
       );
       return data;
     }
